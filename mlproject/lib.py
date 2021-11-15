764a678bbabde89d2d3b9cc8eccbfa559a50038d
Stephane Rongier <stephane.rongier@hotmail.com>
2021-11-15 16:22:19 +0100

    Initial Push

--- mlproject/lib.py
+++ mlproject/lib.py
@@ -2,3 +2,14 @@
 
 def hello_world():
     return "Hello world from mlproject"
+
+
+def try_me() :
+    print("tell me what the fuck")
+    result=input()
+    if result == what the fuck:
+        print("tkt frero")
+    else:
+        print("tell me what the fuck")
+       
+    
